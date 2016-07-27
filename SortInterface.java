b2664c44fca01fddfd477bad42ef392b9ea0bdb7
UVGJoseMart96 <mar15163@uvg.edu.gt>
2016-07-27 10:00:48 -0600

    Add files via upload

---
+++ SortInterface.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jose Martinez, David Sanchinelli, Juan Pablo Zea, Diego Hurtarte
+ */
+public interface SortInterface {
+    
+    public int[] insertionSort();
+    public int[] bubbleSort();
+    public int[] mergeSort();
+    public int[] quickSort();
+    
+}
