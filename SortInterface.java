7186926295141074305716db3e21db0957b40eb1
san15549 <san15549@uvg.edu.gt>
2016-07-27 10:03:51 -0600

    Update SortInterface.java

--- SortInterface.java
+++ SortInterface.java
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- *
  * @author Jose Martinez, David Sanchinelli, Juan Pablo Zea, Diego Hurtarte
+ * @version 31/07/16
  */
 public interface SortInterface {
     
