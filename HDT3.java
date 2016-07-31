e6cd549e6c77944205631e4189c3749c69184c6b
san15549 <san15549@uvg.edu.gt>
2016-07-31 17:21:22 -0600

    Update HDT3.java

--- HDT3.java
+++ HDT3.java
@@ -14,11 +14,17 @@ public class HDT3 {
         Sort sorting = new Sort(lista);
         System.out.println("Bienvenido. Seleccione el numero del metodo de ordenamiento deseado:\n----------\n1.Insertion Sort\n2.Bubble Sort\n3.Merge Sort\n4.Quick Sort\n----------");
         String comando = teclado.nextLine();
+        if(comando.equals("1")){
+            sorting.insertionSort();
+        }
         if(comando.equals("2")){
             sorting.bubbleSort();
         }
-        else{
-            System.out.println("Esa operacion no existe.");
+        if(comando.equals("3")){
+            sorting.mergeSort();
+        }
+        if(comando.equals("4")){
+            sorting.quickSort();
         }
         System.out.println(sorting.toString());
         System.out.println("Se ha finalizado el programa.");
