75adf8dc06eb9757fbd634869a3fca2047b82099
hur15022 <hur15022@gmail.com>
2016-07-31 16:30:31 -0600

    Update iSort.java

--- iSort.java
+++ iSort.java
@@ -7,6 +7,6 @@ public interface iSort {
     public int[] insertionSort();
     public int[] bubbleSort();
     public int[] mergeSort();
-    public int[] quickSort(int arrgelo[]);
+    public int[] quickSort();
     
 }
